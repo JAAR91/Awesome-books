4c7007573880004b27e443b1197daa599de3c113
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-08-09 16:48:03 -0300

    Add new book form and book remove buttom

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,37 @@
-import container from './domloader.js';
+import container, { newBookTitle, newBookAuthor, newBookButton } from './domloader.js';
 import myLibrary from './constructor.js';
 
 const loadBooks = () => {
     container.innerHTML = ``;
-    myLibrary.list.forEach(element => {
-        container.innerHTML += `<div>
-            <p>Title: ${element.title} </p>
-            <p>Author: ${element.author} </p>
-        </div>`;
+
+    myLibrary.list.forEach((book) => {
+        let removeButton = document.createElement("button");
+        let innerDiv = document.createElement("div");
+
+        removeButton.addEventListener('click', (event) => {
+            let newList = myLibrary.list.filter((item) => {
+                return !(item.title === book.title && item.author === book.author)
+            });
+            myLibrary.list = newList;
+            loadBooks();
+        });
+        removeButton.textContent = 'Remove';
+
+        innerDiv.innerHTML += `
+            <p>Title: ${book.title} </p>
+            <p>Author: ${book.author} </p>`;
+        innerDiv.appendChild(removeButton);
+        container.appendChild(innerDiv);
     });
 };
 
+newBookButton.addEventListener('click', (event) => {
+    // stop form submission
+    myLibrary.new(newBookTitle.value, newBookAuthor.value);
+    loadBooks();
+
+    event.preventDefault();
+});
 
 myLibrary.new('harry potter', 'J K Rowling');
 myLibrary.new('lords of the rings', 'Tolkien');
