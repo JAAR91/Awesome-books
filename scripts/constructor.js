827dc655b3f3b286a38fe4b75465e8c0d7722594
JAAR91 <jaarriagaramos@gmail.com>
2021-08-10 11:36:50 -0600

    create render method on class Library and add validation to the form

--- scripts/constructor.js
+++ scripts/constructor.js
@@ -1,7 +1,11 @@
+import container from "./domloader.js";
+
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
+    this.print = `<p>Title: ${this.title} </p>
+    <p>Author: ${this.author} </p>`;
   }
 }
 
@@ -17,11 +21,32 @@ class Library {
   addBook(title, author) {
     this.list.push(new Book(title, author));
     this.save();
+    this.render();
   };
 
   deleteBook(index) {
     this.list.splice(index, 1);
     this.save();
+    this.render();
+  }
+
+  render(){
+    container.innerHTML = ``;
+    const booksContainer = document.createElement('ul');
+    this.list.forEach((book, index) => {
+      const bookEntry = document.createElement('li');
+      bookEntry.classList.add('bookEntry');
+      bookEntry.innerHTML = book.print;
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'delete';
+      removeButton.addEventListener('click', (event) => {
+        this.deleteBook(index);
+        event.preventDefault();
+      });
+      bookEntry.appendChild(removeButton);
+      booksContainer.appendChild(bookEntry);
+    })
+    container.appendChild(booksContainer);
   }
 }
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,23 @@
 import container, { newBookTitle, newBookAuthor, newBookButton } from './domloader.js';
 import myLibrary from './constructor.js';
 
-myLibrary.load();
-
-const loadBooks = () => {
-  container.innerHTML = '';
-
-  myLibrary.list.forEach((book, index) => {
-    const removeButton = document.createElement('button');
-    const innerDiv = document.createElement('div');
-    innerDiv.classList.add('innerDiv');
-
-    removeButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      myLibrary.removeBook(index);
-      loadBooks();
-    });
-
-    removeButton.textContent = 'Remove';
-
-    innerDiv.innerHTML += `
-            <p>Title: ${book.title} </p>
-            <p>Author: ${book.author} </p>`;
-    innerDiv.appendChild(removeButton);
-    container.appendChild(innerDiv);
-  });
-  myLibrary.save();
-};
+myLibrary.render();
 
 newBookButton.addEventListener('click', (event) => {
   myLibrary.addBook(newBookTitle.value, newBookAuthor.value);
   newBookTitle.value = '';
   newBookAuthor.value = '';
-  loadBooks();
-  event.preventDefault();
 });
 
-loadBooks();
+const buttonDisabled = () => {
+  if(newBookTitle.value !== '' && newBookAuthor.value !== '') {
+    newBookButton.disabled = false;
+  }
+  else {
+    newBookButton.disabled = true;
+  }
+};
+
+newBookTitle.addEventListener('input', buttonDisabled);
+newBookAuthor.addEventListener('input', buttonDisabled);
+
